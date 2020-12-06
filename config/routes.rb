54450ad045ea577612a973aceac202017f34c273
jesusbaena <jesusdbpascarella02@gmail.com>
2020-12-06 13:35:52 -0400

    Cambiando las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   
   resources :articles do 
     resources :comments
+  
   end 
+  
   root :to => "welcome#index"
   get 'articles/new'
   get 'posts/new'
   get 'posts/create'
   post 'posts/new'
   post 'articles/new'
+  
   get 'articles/id/show'
 end
